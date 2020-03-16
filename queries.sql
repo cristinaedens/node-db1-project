cc130748c1c61097b5c0e93cc08d529a8a1d535b
Cristina Edens <edens.cristina@gmail.com>
2020-03-16 15:13:18 -0400

    Added Bilbo Baggins new customer

--- queries.sql
+++ queries.sql
@@ -19,6 +19,8 @@ SELECT * FROM [Customers]
 WHERE Country = 'Brazil' or City = 'Madrid' or City = 'London';
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+INSERT INTO [Customers] (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
