8b697a6c028f7c359fdfbec49881ba4862bc869f
Cristina Edens <edens.cristina@gmail.com>
2020-03-16 16:31:22 -0400

    get by ID is returning the data from DB

--- accountsRouter/accountsRouter.js
+++ accountsRouter/accountsRouter.js
@@ -15,4 +15,20 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    db('accounts')
+        .where({ id: req.params.id })
+        .first()
+        .then(accounts => {
+            if (accounts) {
+                res.status(200).json({ data: accounts });
+            } else {
+                res.status(404).json({ message: "Account could not be found" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "sorry, there was an error returning the data." });
+        })
+});
+
 module.exports = router;
